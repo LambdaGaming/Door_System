21feda379079e7a28c802689e12384ab9d446a3f
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-01-26 11:48:07 -0500

    Change default menu key to F2

--- lua/autorun/client/cl_door_config.lua
+++ lua/autorun/client/cl_door_config.lua
@@ -1,4 +1,4 @@
-CreateClientConVar( "DoorKey", KEY_F9, true, false, "Key that you press to access the menu of a door." )
+CreateClientConVar( "DoorKey", KEY_F2, true, false, "Key that you press to access the menu of a door." )
 CreateClientConVar( "ShowDoorHealth", "0", true, false, "Whether or not a door's health should appear on the HUD when looking at it." )
 
 hook.Add( "PopulateToolMenu", "DoorConfig", function()
